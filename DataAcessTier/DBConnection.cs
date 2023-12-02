9c9353b4c825761f20ce0eb9335070bc11279325
huumanh\huumanh <tranhuumanh1612@gmail.com>
2023-12-03 04:38:22 +0700

    Add project files.

---
+++ DataAcessTier/DBConnection.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.OleDb;
+
+namespace DataAcessTier
+{
+    public class DBConnection
+    {
+        protected OleDbConnection conn;
+
+        public DBConnection()
+        {
+            try
+            {
+                conn = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=dbSanPham.mdb;Persist Security Info=True");
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+    }
+}
